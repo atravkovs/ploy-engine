69197e3d093b4ea25fc4d2ff5f6622966647e605
Artjoms Travkovs <atravkovs@gmail.com>
2024-01-21 22:29:59 +0200

    add sample configuration and xml parser

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,35 @@
-use std::rc::Rc;
+use anyhow::Result;
+use quick_xml::de::from_str;
+use serde::Deserialize;
+use std::fs;
 
-trait Delegate {
-    fn execute();
-}
-
-trait Step {
-    fn next(&self) -> Vec<Rc<dyn Step>>;
-    fn execute(&self);
-}
-
-#[derive(Clone)]
-struct StartNode {
-    next: Vec<Rc<dyn Step>>,
-}
-
-impl StartNode {
-    fn new(next: Rc<dyn Step>) -> Self {
-        Self { next: vec![next] }
-    }
-}
-
-impl Step for StartNode {
-    fn next(&self) -> Vec<Rc<dyn Step>> {
-        self.next.clone()
-    }
-
-    fn execute(&self) {}
-}
-
-#[derive(Clone)]
-struct EndNode {}
-
-impl EndNode {
-    fn new() -> Self {
-        Self {}
-    }
-}
-
-impl Step for EndNode {
-    fn next(&self) -> Vec<Rc<dyn Step>> {
-        Vec::new()
-    }
-
-    fn execute(&self) {}
-}
-
+#[derive(Deserialize, PartialEq, Debug)]
 struct ActivityNode {
+    #[serde(rename = "@name")]
     name: String,
-    next: Vec<Rc<dyn Step>>,
-    cb: Box<dyn Fn()>,
 }
 
-impl ActivityNode {
-    fn new(name: String, cb: Box<dyn Fn()>, next: Rc<dyn Step>) -> Self {
-        Self {
-            name,
-            cb,
-            next: vec![next],
-        }
-    }
+#[derive(Deserialize, PartialEq, Debug)]
+enum NodeType {
+    StartNode,
+    EndNode,
+    ActivityNode(ActivityNode),
 }
 
-impl Step for ActivityNode {
-    fn next(&self) -> Vec<Rc<dyn Step>> {
-        self.next.clone()
-    }
-
-    fn execute(&self) {
-        println!("I'm executing {}", self.name);
-
-        (self.cb)();
-    }
+#[derive(Deserialize, Debug, PartialEq)]
+struct Nodes {
+    #[serde(rename = "$value")]
+    field: Vec<NodeType>,
 }
 
-fn execute_activity() {
-    println!("Executing an activity");
-}
+fn main() -> Result<()> {
+    let file_name = "Test.ploy";
 
-fn execute_step(step: &Rc<dyn Step>) {
-    step.execute();
+    let file_contents = fs::read_to_string(file_name)?;
 
-    let next_steps = step.next();
+    let nodes: Nodes = from_str(&file_contents)?;
 
-    for next_step in next_steps.iter() {
-        execute_step(next_step);
-    }
-}
+    println!("{:?}", nodes);
 
-fn main() {
-    let end = EndNode::new();
-    let activity2 = ActivityNode::new(
-        "Activity Two".to_string(),
-        Box::new(execute_activity),
-        Rc::new(end),
-    );
-
-    let activity1 = ActivityNode::new(
-        "Activity One".to_string(),
-        Box::new(execute_activity),
-        Rc::new(activity2),
-    );
-
-    let start = StartNode::new(Rc::new(activity1));
-
-    let start_c: Rc<dyn Step> = Rc::new(start);
-
-    execute_step(&start_c);
+    Ok(())
 }
